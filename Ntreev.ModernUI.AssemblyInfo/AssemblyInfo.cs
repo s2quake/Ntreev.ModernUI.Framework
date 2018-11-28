6aba1e10d3bf84e01f434d501a9ee729d9300185
s2quake <han0210@netsgo.com>
2018-11-28 16:16:35 +0900

    4.0.18332.1616

--- Ntreev.ModernUI.AssemblyInfo/AssemblyInfo.cs
+++ Ntreev.ModernUI.AssemblyInfo/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("4.0")]
-[assembly: AssemblyFileVersion("4.0.18326.2023")]
-[assembly: AssemblyInformationalVersion("4.0.18326.2023")]
+[assembly: AssemblyFileVersion("4.0.18332.1616")]
+[assembly: AssemblyInformationalVersion("4.0.18332.1616")]
