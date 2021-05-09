a2ccc53bcf8599ec5f016608899904becb59f5b3
s2quake <han0210@netsgo.com>
2021-05-09 17:04:41 +0900

    new() 간소화

--- JSSoft.ModernUI.Framework/Controls/TerminalColors.cs
+++ JSSoft.ModernUI.Framework/Controls/TerminalColors.cs
@@ -26,42 +26,42 @@ namespace JSSoft.ModernUI.Framework.Controls
 {
     public static class TerminalColors
     {
-        public readonly static ComponentResourceKey ForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(ForegroundKey));
-        public readonly static ComponentResourceKey BackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(BackgroundKey));
+        public readonly static ComponentResourceKey ForegroundKey = new(typeof(TerminalControl), nameof(ForegroundKey));
+        public readonly static ComponentResourceKey BackgroundKey = new(typeof(TerminalControl), nameof(BackgroundKey));
 
-        public readonly static ComponentResourceKey BlackForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(BlackForegroundKey));
-        public readonly static ComponentResourceKey DarkBlueForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkBlueForegroundKey));
-        public readonly static ComponentResourceKey DarkGreenForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkGreenForegroundKey));
-        public readonly static ComponentResourceKey DarkCyanForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkCyanForegroundKey));
-        public readonly static ComponentResourceKey DarkRedForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkRedForegroundKey));
-        public readonly static ComponentResourceKey DarkMagentaForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkMagentaForegroundKey));
-        public readonly static ComponentResourceKey DarkYellowForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkYellowForegroundKey));
-        public readonly static ComponentResourceKey GrayForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(GrayForegroundKey));
-        public readonly static ComponentResourceKey DarkGrayForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkGrayForegroundKey));
-        public readonly static ComponentResourceKey BlueForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(BlueForegroundKey));
-        public readonly static ComponentResourceKey GreenForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(GreenForegroundKey));
-        public readonly static ComponentResourceKey CyanForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(CyanForegroundKey));
-        public readonly static ComponentResourceKey RedForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(RedForegroundKey));
-        public readonly static ComponentResourceKey MagentaForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(MagentaForegroundKey));
-        public readonly static ComponentResourceKey YellowForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(YellowForegroundKey));
-        public readonly static ComponentResourceKey WhiteForegroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(WhiteForegroundKey));
+        public readonly static ComponentResourceKey BlackForegroundKey = new(typeof(TerminalControl), nameof(BlackForegroundKey));
+        public readonly static ComponentResourceKey DarkBlueForegroundKey = new(typeof(TerminalControl), nameof(DarkBlueForegroundKey));
+        public readonly static ComponentResourceKey DarkGreenForegroundKey = new(typeof(TerminalControl), nameof(DarkGreenForegroundKey));
+        public readonly static ComponentResourceKey DarkCyanForegroundKey = new(typeof(TerminalControl), nameof(DarkCyanForegroundKey));
+        public readonly static ComponentResourceKey DarkRedForegroundKey = new(typeof(TerminalControl), nameof(DarkRedForegroundKey));
+        public readonly static ComponentResourceKey DarkMagentaForegroundKey = new(typeof(TerminalControl), nameof(DarkMagentaForegroundKey));
+        public readonly static ComponentResourceKey DarkYellowForegroundKey = new(typeof(TerminalControl), nameof(DarkYellowForegroundKey));
+        public readonly static ComponentResourceKey GrayForegroundKey = new(typeof(TerminalControl), nameof(GrayForegroundKey));
+        public readonly static ComponentResourceKey DarkGrayForegroundKey = new(typeof(TerminalControl), nameof(DarkGrayForegroundKey));
+        public readonly static ComponentResourceKey BlueForegroundKey = new(typeof(TerminalControl), nameof(BlueForegroundKey));
+        public readonly static ComponentResourceKey GreenForegroundKey = new(typeof(TerminalControl), nameof(GreenForegroundKey));
+        public readonly static ComponentResourceKey CyanForegroundKey = new(typeof(TerminalControl), nameof(CyanForegroundKey));
+        public readonly static ComponentResourceKey RedForegroundKey = new(typeof(TerminalControl), nameof(RedForegroundKey));
+        public readonly static ComponentResourceKey MagentaForegroundKey = new(typeof(TerminalControl), nameof(MagentaForegroundKey));
+        public readonly static ComponentResourceKey YellowForegroundKey = new(typeof(TerminalControl), nameof(YellowForegroundKey));
+        public readonly static ComponentResourceKey WhiteForegroundKey = new(typeof(TerminalControl), nameof(WhiteForegroundKey));
 
-        public readonly static ComponentResourceKey BlackBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(BlackBackgroundKey));
-        public readonly static ComponentResourceKey DarkBlueBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkBlueBackgroundKey));
-        public readonly static ComponentResourceKey DarkGreenBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkGreenBackgroundKey));
-        public readonly static ComponentResourceKey DarkCyanBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkCyanBackgroundKey));
-        public readonly static ComponentResourceKey DarkRedBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkRedBackgroundKey));
-        public readonly static ComponentResourceKey DarkMagentaBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkMagentaBackgroundKey));
-        public readonly static ComponentResourceKey DarkYellowBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkYellowBackgroundKey));
-        public readonly static ComponentResourceKey GrayBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(GrayBackgroundKey));
-        public readonly static ComponentResourceKey DarkGrayBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(DarkGrayBackgroundKey));
-        public readonly static ComponentResourceKey BlueBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(BlueBackgroundKey));
-        public readonly static ComponentResourceKey GreenBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(GreenBackgroundKey));
-        public readonly static ComponentResourceKey CyanBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(CyanBackgroundKey));
-        public readonly static ComponentResourceKey RedBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(RedBackgroundKey));
-        public readonly static ComponentResourceKey MagentaBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(MagentaBackgroundKey));
-        public readonly static ComponentResourceKey YellowBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(YellowBackgroundKey));
-        public readonly static ComponentResourceKey WhiteBackgroundKey = new ComponentResourceKey(typeof(TerminalControl), nameof(WhiteBackgroundKey));
+        public readonly static ComponentResourceKey BlackBackgroundKey = new(typeof(TerminalControl), nameof(BlackBackgroundKey));
+        public readonly static ComponentResourceKey DarkBlueBackgroundKey = new(typeof(TerminalControl), nameof(DarkBlueBackgroundKey));
+        public readonly static ComponentResourceKey DarkGreenBackgroundKey = new(typeof(TerminalControl), nameof(DarkGreenBackgroundKey));
+        public readonly static ComponentResourceKey DarkCyanBackgroundKey = new(typeof(TerminalControl), nameof(DarkCyanBackgroundKey));
+        public readonly static ComponentResourceKey DarkRedBackgroundKey = new(typeof(TerminalControl), nameof(DarkRedBackgroundKey));
+        public readonly static ComponentResourceKey DarkMagentaBackgroundKey = new(typeof(TerminalControl), nameof(DarkMagentaBackgroundKey));
+        public readonly static ComponentResourceKey DarkYellowBackgroundKey = new(typeof(TerminalControl), nameof(DarkYellowBackgroundKey));
+        public readonly static ComponentResourceKey GrayBackgroundKey = new(typeof(TerminalControl), nameof(GrayBackgroundKey));
+        public readonly static ComponentResourceKey DarkGrayBackgroundKey = new(typeof(TerminalControl), nameof(DarkGrayBackgroundKey));
+        public readonly static ComponentResourceKey BlueBackgroundKey = new(typeof(TerminalControl), nameof(BlueBackgroundKey));
+        public readonly static ComponentResourceKey GreenBackgroundKey = new(typeof(TerminalControl), nameof(GreenBackgroundKey));
+        public readonly static ComponentResourceKey CyanBackgroundKey = new(typeof(TerminalControl), nameof(CyanBackgroundKey));
+        public readonly static ComponentResourceKey RedBackgroundKey = new(typeof(TerminalControl), nameof(RedBackgroundKey));
+        public readonly static ComponentResourceKey MagentaBackgroundKey = new(typeof(TerminalControl), nameof(MagentaBackgroundKey));
+        public readonly static ComponentResourceKey YellowBackgroundKey = new(typeof(TerminalControl), nameof(YellowBackgroundKey));
+        public readonly static ComponentResourceKey WhiteBackgroundKey = new(typeof(TerminalControl), nameof(WhiteBackgroundKey));
 
         public static ComponentResourceKey FindForegroundKey(ConsoleColor? consoleColor)
         {
